314ccb69e6eb8ee3a53d42ecaa1cb5c0ea74dd4d
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-14 21:13:36 +0530

    updated commands

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -6,8 +6,16 @@ use serenity::prelude::*;
         
 #[command] 
 pub async fn status(ctx: &Context, msg: &Message) -> CommandResult { 
-    let activity = Activity::playing(&msg.content);
+    if let Some(act) = msg.content.strip_prefix("~status"){
+    let act=act.trim();
+    let activity = Activity::playing(act);
+    
         let status = OnlineStatus::Idle;
         ctx.set_presence(Some(activity), status).await;
+        msg.channel_id.say(&ctx.http, format!("Set Status to {}",act)).await?;
+    }
+else {
+        msg.channel_id.say(&ctx.http, format!("Failed to change Status")).await?;
+    }
     Ok(())
 }
