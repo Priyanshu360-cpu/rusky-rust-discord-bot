6e79d3e18254ddba5b19d2b1943fe561034e3756
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-14 11:24:31 +0530

    updated a bit

--- src/commands/reply.rs
+++ src/commands/reply.rs
@@ -1,9 +1,9 @@
-use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command] 
-pub async fn reply(ctx: &Context, msg: &Message) -> CommandResult { 
+pub async fn reply(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult { 
     msg.reply(ctx, &msg.content).await?;
     Ok(())
 }
