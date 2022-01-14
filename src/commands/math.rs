a6f5aeb2851a125035a9b6e43d028ea8e7ce7c17
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-14 12:29:54 +0530

    added match in maths

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -3,13 +3,18 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
-pub async fn multiply(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+pub async fn math(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let one = args.single::<f64>()?;
+    let car=args.single::<char>()?;
     let two = args.single::<f64>()?;
-
-    let product = one * two;
-
-    msg.channel_id.say(&ctx.http, product).await?;
+let finalo = match car{
+    '+'=>one+two,
+    '*'=>one*two,
+    '-'=>one-two,
+    '/'=>one/two,
+    _=>0.0,
+};
+    msg.channel_id.say(&ctx.http, finalo).await?;
 
     Ok(())
 }
