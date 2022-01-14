3d42976c27dd49abfb3864050f44135664f6d9dd
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-14 23:50:13 +0530

    updated commands

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -3,10 +3,11 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let ping = std::time::SystemTime::now().duration_since(msg.timestamp.into())?.as_millis();
 
-    msg.channel_id.send_message(ctx, |m| m.embed(|e| e.title("Ping").description("Rust Framework Ping - 23ms").field(
+    msg.channel_id.send_message(ctx, |m| m.embed(|e| e.title("Ping").description(format!("Rust Framework Ping - {}ms",ping)).field(
         "Discord Api ",
-        "Ping - 12ms",
+        format!("Ping - {}ms",ping),
         false,
     ).colour(0xff0000))).await?;
 
